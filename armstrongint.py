fcbbdbb55cac303706714dcee6e1914b6c1ccfa6
Gayathri-24 <53937901+Gayathri-24@users.noreply.github.com>
2019-08-16 19:34:46 +0530

    Create armstrongint.py

---
+++ armstrongint.py
@@ -0,0 +1,11 @@
+a=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+for num in range(a,b+1):
+    sum=0
+    temp=num
+    while temp>0:
+        digit=temp%10
+        sum+=digit**3
+        temp//=10
+        if num==sum:
+          print(sum)
